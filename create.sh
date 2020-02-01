caccd66086297e5a1270365f0b0b00f20f46ac88
Petr Jahoda <jahoda@zapsi.eu>
2020-02-01 07:14:54 +0100

    update: added minimizing executable file for create.sh
    Took 7 minutes

--- create.sh
+++ create.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+cd linux
+upx rompa_lcd_webservice_linux
+cd ..
 docker rmi -f petrjahoda/rompa_lcd_webservice:latest
 docker build -t petrjahoda/rompa_lcd_webservice:latest .
 docker push petrjahoda/rompa_lcd_webservice:latest
